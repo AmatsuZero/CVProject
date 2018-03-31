b57f94e52cb2cd47c12f3c235a67b53c3cc2d4f1
Daubert <jzh16s@hotmail.com>
2018-03-31 23:59:38 +0800

    ADD: Github heatmap

--- Components/OpenSourceActivity/main.js
+++ Components/OpenSourceActivity/main.js
@@ -2,17 +2,24 @@ import React, { PureComponent } from 'react'
 import {
     Text,
     StyleSheet,
-    View
+    View,
+    Image
 } from 'react-native'
 import { TitleColors } from '../../Util/ProjectColors'
+import {remToPixel} from "../../Util/Convertor";
+
+Image.prefetch('http://ghchart.rshah.org/AmatsuZero')
 
 export default class Github extends PureComponent {
     render() {
         return (
             <View style={Styles.main}>
-                <Text>
+                <Text style={Styles.title}>
                     Github
                 </Text>
+                <Image
+                    style={Styles.heatMap}
+                    source={{uri: "http://ghchart.rshah.org/AmatsuZero"}}/>
             </View>
         )
     }
@@ -21,6 +28,17 @@ export default class Github extends PureComponent {
 const Styles = StyleSheet.create({
     main: {
         flexDirection:'column',
-        marginTop: 10
+        marginTop: 10,
+        justifyContent:'space-between'
+    },
+    title:{
+        marginVertical:5,
+        lineHeight:remToPixel(1.6),
+        fontSize: 15
+    },
+    heatMap:{
+        alignSelf:'stretch',
+        flex:1,
+        height:100
     }
 })
